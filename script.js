575e5083a2d59515cd16674bf75dd232d71519cc
sucharita4867 <sucharitasardar7@gmail.com>
2025-09-08 17:03:47 +0530

    added extra work

--- script.js
+++ script.js
@@ -5,22 +5,63 @@ fetch(url)
 
 const categories = (cats) => {
   const categoriesContainer = document.getElementById("categories-container");
-  categoriesContainer.innerHTML = ""; // clear আগে
+  categoriesContainer.innerHTML = "";
 
   for (let categorie of cats) {
     const categoriesDiv = document.createElement("div");
     categoriesDiv.innerHTML = `
-    <div id="categories-container" class="  hover:text-white col-span-2 text-[#1f2937] hover:bg-[#15803D] rounded-sm space-y-4 py-2  text-center bg-[#cff0dc] font-semibold block">
+    <div id="categories-container-${categorie.id}" class="  hover:text-white col-span-2 text-[#1f2937] hover:bg-[#2daf59] rounded-sm space-y-4 py-2  text-center bg-[#cff0dc] font-semibold block cat-btn">
                                <p>${categorie.category_name}</p>
                          </div>
      `;
+    const removeActive = () => {
+      const categoryBtn = document.querySelectorAll(".cat-btn");
+      // console.log(categoryBtn);
+      categoryBtn.forEach((btn) => btn.classList.remove("active"));
+    };
     categoriesDiv.addEventListener("click", () => {
       const url = `https://openapi.programming-hero.com/api/category/${categorie.id}`;
       console.log(url);
       fetch(url)
-        .then((res) => res.json)
-        .then((data) => console.log(data));
+        .then((res) => res.json())
+        .then((data) => {
+          removeActive();
+          const clickBtn = document.getElementById(
+            `categories-container-${categorie.id}`
+          );
+          clickBtn.classList.add("active");
+          categoryItem(data.plants);
+        });
     });
+    const categoryItem = (items) => {
+      const cardBox = document.getElementById("card-container");
+      cardBox.innerHTML = "";
+      items.forEach((cardItem) => {
+        // console.log(item);
+        const card = document.createElement("div");
+        card.innerHTML = `
+    
+    <div class="bg-white md:p-3 p-1 space-y-3 rounded-md">
+                                    <img class="rounded-md" src="${cardItem.image}" alt="">
+                                    <div class="space-y-2">
+                                          <h1 class="font-semibold">${cardItem.name}</h1>
+                                          <p class="text-[#1f2937] text-sm">${cardItem.description}</p>
+                                          <div class="flex justify-between items-center">
+                                                <p class="text-[#15803d] bg-[#dcfce7] md:px-2 md:py-1 rounded-full">${cardItem.category}
+                                                </p>
+                                                <p class="font-semibold">${cardItem.price}</p>
+                                          </div>
+                                          <div
+                                                class="w-full text-center bg-[#15803D] text-white md:px-6 md:py-3 px-2 py-1 rounded-full hover:opacity-85">
+                                                <button class="">Add to Cart</button>
+                                          </div>
+                                    </div>
+                              </div>
+    `;
+        cardBox.append(card);
+      });
+    };
+
     categoriesContainer.append(categoriesDiv);
   }
 };
